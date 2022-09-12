c2fc28ed974d2643b4b78e7644fb804f977ab0da
tcarlucc <tcarlucc@uvm.edu>
2022-09-11 20:39:51 -0400

    Access functionaliy works.

--- main.py
+++ main.py
@@ -4,6 +4,8 @@ Thomas Carlucci
 CS 166 / Fall 2022
 """
 import csv
+import user
+
 from user import User
 
 def main():
@@ -18,21 +20,47 @@ def main():
 
     print("Welcome to Unsecure Company Intranet!")
     while not valid_login:
-        username = input("Please enter your username to begin:")
+        username = input("Please enter your username:")
         password = input("Please enter your password:")
 
         for login in logins:
             if username == login.get_username() and password == login.get_password():
                 temp_user = login
                 valid_login = True
-            else:
-                print("Username or Password incorrect, please try again.")
+        if not valid_login:
+            print("Username or Password incorrect, please try again.")
 
     active_user = temp_user
+    menu_loop = True
+
+    while menu_loop:
+        print_menu()
+        option = int(input("Choose an option listed above:"))
+        if option == 1:
+            if active_user.get_access() == "ADMIN":
+                print("!!! You have now accessed the time reporting application")
+            else:
+                print("!!! You are not authorized to access this area.")
+        elif option == 2:
+            if active_user.get_access() == "ADMIN" or active_user.get_access() == "ADVANCED":
+                print("!!! You have now accessed the accounting application")
+            else:
+                print("!!! You are not authorized to access this area.")
+        elif option == 3:
+            print("!!! You have now accessed the developer environment.")
+        else:
+            print("Invalid option. Try again.")
 
     print("Welcome, " + temp_user.get_username() + "!")
 
 
+def print_menu():
+    print("Press a number to begin:")
+    print("-------------------------")
+    print("1: Time Reporting")         # ADMIN
+    print("2: Accounting")             # ADVANCED
+    print("3: Developer Environment")  # BASIC
+    print("-------------------------")
 
 
 if __name__ == "__main__":
