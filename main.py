cca96bec0f84ff4c6876b029bc2a4e94a088b38f
tcarlucc <tcarlucc@uvm.edu>
2022-09-11 20:03:40 -0400

    Fixed user import, added opening of csv functionality and can add users from said file

--- main.py
+++ main.py
@@ -3,9 +3,28 @@ Lab 1
 Thomas Carlucci
 CS 166 / Fall 2022
 """
-import User
+import csv
+from user import User
 
 def main():
+
+    logins = []
+    valid_login = False
+
+    with open('userdata.csv', 'r') as f:
+        database = csv.reader(f)
+        for row in database:
+            new_user = User(row[0], row[1], row[2])
+            logins.append(new_user)
+
+
+    while not valid_login:
+        print("Welcome to Unsecure Company Intranet!")
+        username = input("Please enter your username to begin:")
+        password = input("Please enter your password:")
+        print(new_user)
+
+
     pass
 
 if __name__ == "__main__":
